1e987e05a8efa2777d1418066713eeb10b48d306
Aarav Bodla <147215351+aaravbodla@users.noreply.github.com>
2025-06-11 23:00:32 +0530

    Create BT_Right_Side_View.java

---
+++ BT_Right_Side_View.java
@@ -0,0 +1,17 @@
+//Time Complexity - O(n)
+//Space complexity - O(1)
+class Solution {
+    private List<Integer> answer = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        helper(root,1);
+        return answer;
+    }
+
+    private void helper(TreeNode root,int level){
+        if(root == null) return;
+        if(level > answer.size()) answer.add(root.val);
+
+        helper(root.right,level+1);
+        helper(root.left,level+1);
+    }
+}
